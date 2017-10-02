be2d9add1393bf1d04450148b17d0a470dec96a1
Velisha <velishavaghela@gmail.com>
2017-10-02 14:09:43 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+(function() {
+   var mainapp = angular.module("myapp",["ngRoute"]);
+mainapp.config (function($routeProvider){
+//    console.log('routeProvider')
+    $routeProvider
+    /*.when("/",{
+        templateUrl:"/"
+    })*/
+    .when("/about",{
+        templateUrl:"templates/about.html"
+    })
+    .when("/skills",{
+        templateUrl:"templates/skills.html"
+    })
+    .when("/work",{
+        templateUrl:"templates/work.html"
+    })
+     .when("/contact",{
+        templateUrl:"templates/contact.html"
+    })
+    .otherwise({
+        redirectTo:"/",
+    });
+});
+})();
